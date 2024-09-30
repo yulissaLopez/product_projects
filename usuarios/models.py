b01536276e2f9e22f06f53ac585ce34bbd7a078b
yulissaLopez <yulissa.lopezcortes@hotmail.com>
2024-09-30 11:28:27 -0500

    user get method

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Usuario(AbstractUser):
+    # se anaden los campos personalizados
     direccion = models.CharField(100, null=True, blank=True, max_length=100)
     pais = models.CharField(max_length=100, default="COLOMBIA")
     
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Usuario
+from django.core.serializers import serialize
+import json
 
 # Create your views here.
-def index():
-    pass
+def index(request):
+    #Metodo GET
+    if request.method == 'GET':
+        list_usuarios = Usuario.objects.all()
+        usuarios_json = serialize('json', list_usuarios)
+        dict_usuarios = json.loads(usuarios_json)
+        return JsonResponse(dict_usuarios, safe=False)
+
+    #MEtodo POST
+    #Metodo PUT
+    #Metodo DELETE
+    
